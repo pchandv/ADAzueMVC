de15cf853b12d68f600562fe3567294ba6131266
Praveen Visam <Praveen.Visam@sbdinc.com>
2020-04-01 14:09:41 +0530

    Initial Check in

---
+++ ADMVC/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        //https://docs.microsoft.com/en-us/azure/active-directory/develop/tutorial-v2-asp-webapp
+        //https://www.c-sharpcorner.com/article/azure-ad-authentication-for-mvc-web-application/
+        // Sends an OpenIDConnect Sign-In Request.  
+        public void SignIn()
+        {
+            // Send an OpenID Connect sign-in request.
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        public void SignOut()
+        {
+            string callbackUrl = Url.Action("SignOutCallback", "Account", routeValues: null, protocol: Request.Url.Scheme);
+
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                new AuthenticationProperties { RedirectUri = callbackUrl },
+                OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+        }
+
+        public ActionResult SignOutCallback()
+        {
+            if (Request.IsAuthenticated)
+            {
+                // Redirect to home page if the user is authenticated.
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ADMVCAuto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ADMVCAuto.MvcApplication" Language="C#" %>
